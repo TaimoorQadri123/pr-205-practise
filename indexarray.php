dc72b07b1e19b09841f559d21b3ae9938e3dbe07
STUDENT <std1@LABSFC.COM>
2025-02-26 23:30:37 -0800

    third php class done

---
+++ indexarray.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+
+    <div class="container">
+        <?php 
+        $students = ["Ali","Aqsa","Hamza","Sana"];
+        print_r($students);
+
+        ?>
+        
+        <ul>
+            <?php
+            for($i=0 : $i<count($students) : $i++ ){
+                ?>
+             <li> <?php echo $students[$i] ?> </li>
+
+            
+            <?php
+            }
+            ?>
+        </ul>
+
+        <h1>foreach loop</h1>
+
+
+        <ul>
+            <?php
+            foreach($students as $std){
+                ?>
+             <li> <?php echo $std ?> </li>
+
+            
+            <?php
+            }
+            ?>
+        </ul>
+
+
+    </div>
+      
+    </body>
+</html>
+        
