4f06da820c0ff4635da55679f0fe38d6c5211a85
TaimoorQadri123 <dchota374@gmail.com>
2025-03-08 02:07:52 +0500

    str_functions done

---
+++ str_functions.php
@@ -0,0 +1,40 @@
+<?php
+$str = "Hello thIs is Ali. he is intelligent , Ali is excellent";
+echo strlen($str) . "<br>";
+echo str_word_count($str)."<br>";
+echo ucwords($str)."<br>";
+echo strtolower($str)."<br>";
+echo strtoupper($str)."<br>";
+echo "<br>";
+echo $str."<br>";
+echo str_replace("Ali",'Huzaifa',$str)."<br>";
+$str2 = "hello this is Ali";
+print_r(explode(' ',$str2)) . "<br>";
+$url = "https/mkp/str_functions.php";
+// print_r(explode("/",$url));
+$urlArry = explode('/',$url); // return arrayArray ( [0] => https [1] => PR2-202405C [2]  =>functions [3] => str_fun.php )
+echo $urlArry[count($urlArry)-1]; // [4]
+echo "<br>";
+$fileName = "index.html";
+print_r(explode('.',$fileName));
+echo "<br>";
+$fileNameArray = explode('.',$fileName) ;
+echo $fileNameArray[count($fileNameArray)-1] ."<br>";
+
+$name = "ali khan";
+echo strrev($name);
+
+echo "<br>" ;
+$empName = "ali";
+echo strcmp('ali',$empName);
+
+?>
+<br>
+<?php
+$fileName = "index.html";
+print_r(explode('.',$fileName));
+
+echo "<br>";
+$fileNameArray = explode('.',$fileName);
+echo $fileNameArray[count($fileNameArray)-1] ."<br>"; 
+?>
