e19cca0ddcbdd688c23eaf9bd6ca949a6884212d
TaimoorQadri123 <dchota374@gmail.com>
2025-03-19 09:34:06 +0500

    crud done

--- crud/query.php
+++ crud/query.php
@@ -14,6 +14,16 @@ if(isset($_POST['registerUser'])){
     }
     if(empty($userEmail)){
         $userEmailErr = "Email is Required";
+    }else{
+        $query = $pdo->prepare("select * from user where email = :uEmail ");
+        $query->bindParam('uEmail',$userEmail);
+        $query->execute();
+        $user = $query->fetch(PDO::FETCH_ASSOC);
+        // print_r($user);
+        // die();
+        if($user){
+            $userEmailErr = "Email is already Exist";
+        }
     }
     if(empty($userPassword)){
         $userPasswordErr = "Password is Required";
@@ -21,8 +31,76 @@ if(isset($_POST['registerUser'])){
     if(empty($userConfirmPassword)){
         $userConfirmPasswordErr = "Conform Password is Required";
     }
+    else{
+        if($userPassword != $userConfirmPassword){
+            $userConfirmPasswordErr = "password not matched";
+        }
+    }
+
+ if(empty($userNameErr) && empty($userEmailErr) && empty($userPasswordErr) && empty($userConfirmPasswordErr)){
+    $query = $pdo->prepare("insert into user(name,email,pssword) values (:uName , :uEmail , :uPassword)");
+    $query->bindParam('uName',$userName);
+    $query->bindParam('uEmail',$userEmail);
+    $query->bindParam('uPassword',$userPassword);
+    $query->execute();
+    // echo "<script>alert('user added ');location.assign('register.php')</script>":
+    echo "<script>alert('User added successfully'); location.assign('register.php');</script>";
+
+ }
 
 
 }
+
+
+// include("dbcon.php");
+
+// $userName = $userEmail = $userPassword = $userConfirmPassword = "";
+// $userNameErr = $userEmailErr = $userPasswordErr = $userConfirmPasswordErr = "";
+
+// if (isset($_POST['registerUser'])) {
+//     $userName = $_POST['uName'];
+//     $userEmail = $_POST['uEmail'];
+//     $userPassword = $_POST['uPassword'];
+//     $userConfirmPassword = $_POST['uConfirmPassword'];
+
+//     if (empty($userName)) {
+//         $userNameErr = "Name is required.";
+//     }
+//     if (empty($userEmail)) {
+//         $userEmailErr = "Email is required.";
+//     } else {
+//         $query = $pdo->prepare("SELECT * FROM users WHERE email = :uEmail");
+//         $query->bindParam(':uEmail', $userEmail);
+//         $query->execute();
+//         $user = $query->fetch(PDO::FETCH_ASSOC);
+//         if ($user) {
+//             $userEmailErr = "Email already exists.";
+//         }
+//     }
+
+//     if (empty($userPassword)) {
+//         $userPasswordErr = "Password is required.";
+//     }
+//     if (empty($userConfirmPassword)) {
+//         $userConfirmPasswordErr = "Confirm password is required.";
+//     } else {
+//         if ($userPassword != $userConfirmPassword) {
+//             $userConfirmPasswordErr = "Passwords do not match.";
+//         }
+//     }
+
+//     if (empty($userNameErr) && empty($userEmailErr) && empty($userPasswordErr) && empty($userConfirmPasswordErr)) {
+//         $hashedPassword = password_hash($userPassword, PASSWORD_DEFAULT);
+//         $query = $pdo->prepare("INSERT INTO users (name, email, password) VALUES (:uName, :uEmail, :uPassword)");
+//         $query->bindParam(':uName', $userName);
+//         $query->bindParam(':uEmail', $userEmail);
+//         $query->bindParam(':uPassword', $hashedPassword);
+//         $query->execute();
+
+//         echo "<script>alert('User added successfully'); location.assign('register.php');</script>";
+//     }
+// }
+
+
 
 ?>
