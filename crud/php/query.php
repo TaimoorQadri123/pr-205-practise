8e25cf9ba2eff7bb23cd0bcd8f48ae9f479234a6
TaimoorQadri123 <dchota374@gmail.com>
2025-03-25 07:47:36 +0500

    role_based_login_system

--- crud/php/query.php
+++ crud/php/query.php
@@ -1,5 +1,6 @@
 <?php
 include("dbcon.php");
+session_start();
 
 
 $userName = $userEmail = $userPassword = $userConfirmPassword = "" ;
@@ -101,6 +102,48 @@ if(isset($_POST['registerUser'])){
 //     }
 // }
 
+if(isset($_POST['login'])){
+    $userEmail = $_POST['uEmail'];
+    $userPassword = $_POST['uPassword'];
+    if(empty($userEmail)){
+        $userEmailErr = "Email s Required";
+    }
+    else{
+        $query = $pdo->prepare("SELECT * FROM user WHERE email = :uEmail");
+        $query->bindParam(':uEmail',$userEmail);
+        $query->execute();
+        $user = $query->fetch(PDO::FETCH_ASSOC);
+        if($user){
+            if($user['pssword'] == $userPassword){
+                if($user['role_id'] == 1){
+                    $_SESSION['adminId'] = $user['id'];
+                    $_SESSION['adminEmail'] = $user['email'];
+                    $_SESSION['adminName'] = $user['name'];
+                    $_SESSION['adminRole'] = $user['role_id'];
+
+                    echo "<script>location.assign('adminPanel/index.php?success=login successfully admin')</script>";
+                }
+                else if($user['role_id'] == 2){
+                    $_SESSION['userId'] = $user['id'];
+                    $_SESSION['userEmail'] = $user['email'];
+                    $_SESSION['userName'] = $user['name'];
+                    $_SESSION['userRole'] = $user['role_id'];
+
+                    echo "<script>location.assign('index.php?success=login successfully user')</script>";
+                }
+            }
+            else{
+                $userPasswordErr = "password does not matched";
+            }
+        }
+        else{
+            $userEmailErr = "user not found";
+        }
+    }
+    if(empty($userPassword)){
+        $userPasswordErr = "password is required";
+    }
+}
 
 
 ?>
