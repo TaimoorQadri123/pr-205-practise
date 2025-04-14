55c535697bba2b67b5f11bd9dd1aafb6cb3472be
TaimoorQadri123 <dchota374@gmail.com>
2025-04-15 02:08:35 +0500

    with database

--- basic php/db/apt.sql
+++ basic php/db/apt.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2025 at 06:53 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Apr 14, 2025 at 11:07 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `aptech`
+-- Database: `apt`
 --
 
 -- --------------------------------------------------------
@@ -32,7 +32,31 @@ CREATE TABLE `categories` (
   `name` varchar(200) NOT NULL,
   `description` varchar(200) NOT NULL,
   `image` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `description`, `image`) VALUES
+(1, 'shoes', 'Khaki Comfy Slip On Sneakers 0120567', 'shoes.webp'),
+(2, 'bags', 'Fashion Man Small Chest Bag Phone Pocket Cross Body Shoulder Fanny Pack Male Handbag Outdoor Neck Side Crossbody Gym Bags', 'bagss.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `price` int(11) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `image` varchar(500) NOT NULL,
+  `c_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -43,7 +67,7 @@ CREATE TABLE `categories` (
 CREATE TABLE `role` (
   `id` int(11) NOT NULL,
   `name` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `role`
@@ -65,7 +89,7 @@ CREATE TABLE `user` (
   `email` varchar(200) NOT NULL,
   `pssword` varchar(200) NOT NULL,
   `role_id` int(11) NOT NULL DEFAULT 2
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `user`
@@ -73,9 +97,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `name`, `email`, `pssword`, `role_id`) VALUES
 (2, 'admin', 'admin@gmail.com', '123', 1),
-(3, 'taimoor ', 'taimoor@gmail.com', '123', 2),
-(4, 'taimoor', 'ali@gmail.com', '123', 2),
-(5, 'haider', 'haider@gmail.com', '1234', 2);
+(3, 'taimor', 'taimoor@gmail.com', 'hvc', 2);
 
 --
 -- Indexes for dumped tables
@@ -86,6 +108,13 @@ INSERT INTO `user` (`id`, `name`, `email`, `pssword`, `role_id`) VALUES
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `c_id` (`c_id`);
 
 --
 -- Indexes for table `role`
@@ -108,6 +137,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -120,11 +155,17 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`c_id`) REFERENCES `categories` (`id`);
 
 --
 -- Constraints for table `user`
